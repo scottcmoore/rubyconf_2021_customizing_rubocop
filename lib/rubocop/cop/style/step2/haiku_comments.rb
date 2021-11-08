f33d8ba414144b5c16eb36e21562ad7afedc019f
Scott Moore <smoore@sondermind.com>
2021-11-07 20:16:08 -0700

    Improve Step 2 instructions.

--- lib/rubocop/cop/style/step2/haiku_comments.rb
+++ lib/rubocop/cop/style/step2/haiku_comments.rb
@@ -20,26 +20,17 @@ module RuboCop
           # TODO: First, print the node name (the name of the defined method). The node should
           # have a method_name method.
 
-          ########### Thinking it makes sense to associate your explanations below with
-          ########### explicit TODO's. I interpreted this as needing 
-          ########### to do 2 things (print the node name and add an offense), but there
-          ########### are some intermediate steps as well. Something like....
-          ########### TODO: Grab the ast_with_comments and assign it to a variable
-          ########### TODO: Bail out if the node_with_comments is empty
-          ########### TODO: Assign the node and array of comments to variables
-          ########### This could also be a bit overkill, so an alternative would be to remove 
-          ########### the first TODO and just leave the whole TODO as just "add an offense..."
-          ########### I'm sort of in favor of pseudocode TODOs associated with your explanations.
-          
-          # We still have access to `processed_source.ast_with_comments` here, so we 
-          # can grab the `ast_with_comments` just for the node being checked:
-          # node_with_comments = processed_source.ast_with_comments.select { |source_node, _comment| source_node == node }
-          # We can just return if the node_with_comments is nil -- that just means we're currently
-          # looking at a node without comments. For this workshop, we'll ignore those.
+          # TODO: Get the AST with comments for the node we're currently linting.
+          # We still have access to `processed_source.ast_with_comments` here, so we
+          # can grab the `ast_with_comments` just for the node being checked, by `select`ing from the
+          # AST where the AST node is the node being linted.
 
           # This will give us a hash like: `{ <AST> => [<Parser::Source::Comment>] }`
           # The first, and only, key will be our `node`.
           # The first, and only, value will be our array of comments.
+
+          # TODO: We should just return if the node_with_comments is empty -- that just means we're currently
+          # looking at a node without comments. For this workshop, we'll ignore those.
 
           # TODO: Second, add an offense if the method definition doesn't have three comment lines.
           # Adding an offense is just `add_offense(node, message: "a helpful message")`.
